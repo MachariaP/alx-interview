a2a5e35c54ec0be1948d33464deca253fbb92ffb
MachariaP <walburphinehas78@gmail.com>
2024-07-04 11:33:59 +0300

    Conditions Validation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,46 @@
+#!/usr/bin/python3
+
+"""
+Module for a method that determines if all the boxes in a list can be unlocked.
+"""
+
+
 def canUnlockAll(boxes):
     """
-    Determine if all boxes can be opened.
+    Determines if all the boxes in the list can be unlocked.
+
     Args:
-      boxes (list of list of int): A list where each element is a list of keys.
+        boxes (list): A list of lists representing the lockboxes
+        and their corresponding keys.
 
     Returns:
-      bool: True if all boxes can be opened, False otherwise.
+      bool: True if all the boxes can be unlocked, False otherwise.
     """
 
-    # Input validation
-    if not boxes or not all(isinstance(box, list) for box in boxes):
+    if not boxes:
         return False
 
-    unlocked = [False] * len(boxes)  # Track which boxes have been unlocked
-    unlocked[0] = True  # The first box is always unlocked
-    keys = set(boxes[0])  # Start with keys from the first box
+    # Create a list to keep track of visited boxes
+    visited = [False] * len(boxes)
+    visited[0] = True
 
-    while keys:
-        new_keys = set()
-        for key in keys:
-            if key < len(boxes) and not unlocked[key]:
-                unlocked[key] = True  # Mark the box as unlocked
-                new_keys.update(boxes[key])  # Add new keys from this box
-        if not new_keys:
-            break  # Exit if no new keys were found
-        keys = new_keys
+    # Create a stack to store boxes to be visited
+    stack = [0]
 
-    return all(unlocked)
+    # Perform depth-first search to visit all boxes
+    while stack:
+        # Pop the top box from the stack
+        box = stack.pop()
+
+        # Iterate through the keys in the current box
+        for key in boxes[box]:
+            # Check if the key is valid and the corresponding box
+            # has not been visited
+            if 0 <= key < len(boxes) and not visited[key]:
+                # Mark the box as visited
+                visited[key] = True
+                # Add the box to the stack for further exploration
+                stack.append(key)
+
+    # Check if all boxes have been visited
+    return all(visited)
